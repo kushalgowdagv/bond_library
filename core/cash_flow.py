626832c003eca3f5f1f0183c0c543c90e00b2677
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2025-04-21 02:06:04 -0400

    " discrete compunding "

--- core/cash_flow.py
+++ core/cash_flow.py
@@ -1,10 +1,11 @@
 """
-Cash flow models and calculations
+Cash flow models and calculations.
 """
 
 from datetime import datetime
 from typing import List, Dict, Optional
 from core.interest_rate import InterestRateCurve
+from core.date_utils import DateUtils
 
 class CashFlow:
     """Base class for cash flows"""
@@ -20,25 +21,9 @@ class CashFlow:
         self.payment_date = payment_date
         self.amount = amount
     
-    # def present_value(self, valuation_date: datetime, curve: InterestRateCurve) -> float:
-    #     """
-    #     Calculate present value of the cash flow
-        
-    #     Args:
-    #         valuation_date: Date for which to calculate present value
-    #         curve: Interest rate curve for discounting
-            
-    #     Returns:
-    #         Present value of the cash flow
-    #     """
-    #     if self.payment_date <= valuation_date:
-    #         return 0.0
-        
-    #     discount_factor = curve.get_discount_factor(valuation_date, self.payment_date)
-    #     return self.amount * discount_factor
     def present_value(self, valuation_date: datetime, curve: InterestRateCurve) -> float:
         """
-        Calculate present value of the cash flow
+        Calculate present value of the cash flow using discrete compounding
         
         Args:
             valuation_date: Date for which to calculate present value
@@ -50,6 +35,7 @@ class CashFlow:
         if self.payment_date <= valuation_date:
             return 0.0
         
+        # Get discount factor from the curve
+        # Note: We need to modify the InterestRateCurve class to use discrete compounding too
         discount_factor = curve.get_discount_factor(valuation_date, self.payment_date)
         return self.amount * discount_factor
-    
